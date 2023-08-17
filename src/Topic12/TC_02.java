0c4774df3e6c312bf3a2ccd53b6febc390be3318
hoangminh <hoangminh.nguyen7377@gmail.com>
2023-08-17 17:53:20 +0700

    cong ty

--- src/Topic12/TC_02.java
+++ src/Topic12/TC_02.java
@@ -4,6 +4,7 @@ public class TC_02 {
     public static void main(String[] args) {
         String statement = "Automation Testing 345 Tutorials Online 789";
         int countA=0;
+        int countNumber=0;
 
         for(int i=0; i<statement.length();i++)
         {
@@ -12,10 +13,21 @@ public class TC_02 {
             if (statement.toLowerCase().charAt(i)=='a')
                 countA++;
         }
+
+        for(int i=0; i<statement.length();i++)
+        {
+            if (Character.isDigit(statement.charAt(i)))
+                countNumber++;
+        }
+
         System.out.println("Tổng số chữ a: "+countA);
 
         System.out.println("Chứa chữ Testing: "+statement.contains("Testing"));
         System.out.println("Bắt đầu bằng Automation: "+statement.startsWith("Automation"));
+        System.out.println("Kết thúc bằng Online: "+statement.endsWith("Online"));
+        System.out.println("Vị trí của Tutorials: "+statement.indexOf("Tutorials"));
+        System.out.println(statement.replace("Online","Offline"));
+        System.out.println("số lượng số: " +countNumber);
 
 
     }
